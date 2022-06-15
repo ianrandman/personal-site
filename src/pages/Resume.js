4f8890d6ca0b3687d7344d094fd3749dd7316b69
Ian Randman <ixr5487@g.rit.edu>
2022-06-14 22:19:19 -0400

    Resume and some other personalization

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,52 +1,68 @@
+/* eslint-disable */
+
 import React from 'react';
+// import ReactPDF from '@react-pdf/renderer';
+import { Document, pdfjs, Page } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
 
-import Education from '../components/Resume/Education';
-import Experience from '../components/Resume/Experience';
-import Skills from '../components/Resume/Skills';
-import Courses from '../components/Resume/Courses';
-import References from '../components/Resume/References';
-
-import courses from '../data/resume/courses';
-import degrees from '../data/resume/degrees';
-import positions from '../data/resume/positions';
-import { skills, categories } from '../data/resume/skills';
-
-const sections = [
-  'Education',
-  'Experience',
-  'Skills',
-  'Courses',
-  'References',
-];
+// import Education from '../components/Resume/Education';
+// import Experience from '../components/Resume/Experience';
+// import Skills from '../components/Resume/Skills';
+// import Courses from '../components/Resume/Courses';
+// import References from '../components/Resume/References';
+//
+// import courses from '../data/resume/courses';
+// import degrees from '../data/resume/degrees';
+// import positions from '../data/resume/positions';
+// import { skills, categories } from '../data/resume/skills';
+//
+// const sections = [
+//   'Education',
+//   'Experience',
+//   'Skills',
+//   'Courses',
+//   'References',
+// ];
 
 const Resume = () => (
   <Main
     title="Resume"
-    description="Michael D'Angelo's Resume. Arthena, Matroid, YC, Skeptical Investments, Stanford ICME, Planet Labs, and Facebook."
+    // description="Michael D'Angelo's Resume. Arthena, Matroid, YC, Skeptical Investments, Stanford ICME, Planet Labs, and Facebook."
   >
-    <article className="post" id="resume">
-      <header>
-        <div className="title">
-          <h2 data-testid="heading"><Link to="resume">Resume</Link></h2>
-          <div className="link-container">
-            {sections.map((sec) => (
-              <h4 key={sec}>
-                <a href={`#${sec.toLowerCase()}`}>{sec}</a>
-              </h4>))}
-          </div>
+    {/*<embed src={{Pdf}} type="application/pdf"/>*/}
+    {/*<Page pageNumber={1} />*/}
+    <Document file="/Ian Randman Resume.pdf">
+      <Page pageNumber={1} />
+    </Document>
+    {/*<ReactPDF*/}
+    {/*  file={{*/}
+    {/*    data: Pdf*/}
+    {/*  }}*/}
+    {/*/>*/}
+    {/*<article className="post" id="resume">*/}
+    {/*  <header>*/}
+    {/*    <div className="title">*/}
+    {/*      <h2 data-testid="heading"><Link to="resume">Resume</Link></h2>*/}
+    {/*      <div className="link-container">*/}
+    {/*        {sections.map((sec) => (*/}
+    {/*          <h4 key={sec}>*/}
+    {/*            <a href={`#${sec.toLowerCase()}`}>{sec}</a>*/}
+    {/*          </h4>))}*/}
+    {/*      </div>*/}
 
-        </div>
-      </header>
-      <Education data={degrees} />
-      <Experience data={positions} />
-      <Skills skills={skills} categories={categories} />
-      <Courses data={courses} />
-      <References />
+    {/*    </div>*/}
+    {/*  </header>*/}
+    {/*  <Education data={degrees} />*/}
+    {/*  <Experience data={positions} />*/}
+    {/*  <Skills skills={skills} categories={categories} />*/}
+    {/*  <Courses data={courses} />*/}
+    {/*  <References />*/}
 
-    </article>
+    {/*</article>*/}
   </Main>
 );
 
