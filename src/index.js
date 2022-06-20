6cb6dd6e9caa1fe90526cc9cbc82ef570c921285
Ian Randman <ixr5487@g.rit.edu>
2022-06-20 19:39:55 -0400

    Added envs

--- src/index.js
+++ src/index.js
@@ -17,3 +17,19 @@ if (rootElement.hasChildNodes()) {
 } else {
   render(<StrictApp />, rootElement);
 }
+
+// remember the original fetch-function to delegate to
+const originalFetch = global.fetch;
+
+const applyBaseUrlToFetch = (baseUrl) => {
+  // replace the global fetch() with our version where we prefix the given URL with a baseUrl
+  global.fetch = (url, options) => {
+    const finalUrl = baseUrl + url;
+    return originalFetch(finalUrl, options);
+  };
+};
+
+// If we have a differing backend configured, replace the global fetch()
+if (process.env.REACT_APP_BACKEND_API_BASE_URL !== undefined && process.env.REACT_APP_BACKEND_API_BASE_URL !== '') {
+  applyBaseUrlToFetch(process.env.REACT_APP_BACKEND_API_BASE_URL);
+}
