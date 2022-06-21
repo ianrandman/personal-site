da6c384ecef0d8cde6b143631abe5c03dc7874e9
Ian Randman <ixr5487@g.rit.edu>
2022-06-21 16:12:55 -0400

    update rate limiter

--- server/init_db.py
+++ server/init_db.py
@@ -1,9 +1,18 @@
+from flask import request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
 
+
+def limiter_helper():
+    if 'X-Real-IP' in request.headers:
+        return request.headers['X-Real-IP']
+    else:
+        return get_remote_address()
+
+
 db = SQLAlchemy()
 limiter = Limiter(
-    key_func=get_remote_address,
+    key_func=limiter_helper,
     default_limits=['20 per minute']
 )
