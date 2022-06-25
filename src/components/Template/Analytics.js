d4b44f76fa21651363959638fcd13fcfa317684e
Ian Randman <ixr5487@g.rit.edu>
2022-06-25 16:01:15 -0400

    Google Analytics?

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -2,19 +2,21 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
-const { NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
+const { REACT_APP_NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
 
-if (NODE_ENV === 'production') {
-  ReactGA.initialize({
-    trackingId: REACT_APP_GA_TRACKING_ID,
-  });
+if (REACT_APP_NODE_ENV === 'production') {
+  ReactGA.initialize([
+    {
+      trackingId: REACT_APP_GA_TRACKING_ID,
+    },
+  ]);
 }
 
 const Analytics = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    if (NODE_ENV === 'production') {
+    if (REACT_APP_NODE_ENV === 'production') {
       ReactGA.send({
         hitType: 'pageview',
         page: pathname,
