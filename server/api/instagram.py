5c70f82ea054ce302c31bc73ee8a5c29f56e218e
Ian Randman <ixr5487@g.rit.edu>
2022-08-02 19:29:43 -0400

    Delete recent Strava activity

--- server/api/instagram.py
+++ server/api/instagram.py
@@ -10,9 +10,12 @@ class InstagramResource(Resource):
 
     def get(self):
         instagram_highlight_obj = get_instagram_highlight()
+
+        highlight_ids = ['17880159521677171', '18136715626281540', '17871099320696462']
+        urls = [f'https://instastories.watch/api/profile/highlight_items?highlightId=highlight%3A{id}' for id in highlight_ids]
+
         # if (datetime.now() - instagram_highlight_obj.time_fetched).seconds / 3600 > 12:
-        url = 'https://instastories.watch/api/profile/highlight_items?highlightId=highlight%3A17880159521677171'
-        success, _ = update_instagram_highlight(url)
+        success, _ = update_instagram_highlight(urls)
         if success:
             instagram_highlight_obj = get_instagram_highlight()
 
--- server/util/update_instagram.py
+++ server/util/update_instagram.py
@@ -6,40 +6,47 @@ from init_db import db
 from util.model import InstagramHighlight
 
 
-def update_instagram_highlight(url):
-    request = requests.get(url)
+def update_instagram_highlight(urls):
+    instagram_highlight_obj = InstagramHighlight.query.limit(1).all()[0]
+    jsons = list()
 
-    try:
-        if request.status_code == 200:
-            # remove videos
-            json = request.json()
+    for url in urls:
+        request = requests.get(url)
 
-            result = dict()
-            result['data'] = [data for data in json if data['type'] == 'photo' or data['type'] == 'video']
-            for media in result['data']:
-                media['url'] = 'https://instastories.watch' + media['url']
-                if media['type'] != 'video':
-                    media['originalUrl'] = 'https://instastories.watch' + media['originalUrl']
+        try:
+            if request.status_code == 200:
+                # remove videos
+                json = request.json()
 
-            # for media in json['data']:
-            #     media['thumbnail'] = media['thumbnail'][27:]
-            #     media['source'] = media['source'][27:]
+                result = dict()
+                result['data'] = [data for data in json if data['type'] == 'photo' or data['type'] == 'video']
+                for media in result['data']:
+                    media['url'] = 'https://instastories.watch' + media['url']
+                    if media['type'] != 'video':
+                        media['originalUrl'] = 'https://instastories.watch' + media['originalUrl']
 
-            instagram_highlight_obj = InstagramHighlight.query.limit(1).all()[0]
-            instagram_highlight_obj.json = result
-            instagram_highlight_obj.time_fetched = datetime.now()
+                # for media in json['data']:
+                #     media['thumbnail'] = media['thumbnail'][27:]
+                #     media['source'] = media['source'][27:]
 
-            db.session.commit()
-        else:
+                jsons.append(result)
+
+            else:
+                error = dict()
+                error['url'] = url
+                error['result'] = request.json()
+                return False, error
+        except Exception as e:
+            print(e)
             error = dict()
             error['url'] = url
             error['result'] = request.json()
             return False, error
-    except Exception as e:
-        print(e)
-        error = dict()
-        error['url'] = url
-        error['result'] = request.json()
-        return False, error
+
+    instagram_highlight_obj.json = {
+        'data': [item for sublist in jsons for item in sublist['data']]
+    }
+    instagram_highlight_obj.time_fetched = datetime.now()
+    db.session.commit()
 
     return True, None
