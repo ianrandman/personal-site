42308db2f0b71749477ad3fbab901390fd457296
Ian Randman <ixr5487@g.rit.edu>
2023-12-10 21:06:28 -0800

    Potentially fix SEO with canonical URL.

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import Analytics from '../components/Template/Analytics';
 import Navigation from '../components/Template/Navigation';
 import SideBar from '../components/Template/SideBar';
-import ScrollToTop from '../components/Template/ScrollToTop';
+// import ScrollToTop from '../components/Template/ScrollToTop';
 
-const Main = (props) => (
-  <HelmetProvider>
-    <Analytics />
-    <ScrollToTop />
-    <Helmet titleTemplate="%s | Ian Randman" defaultTitle="Ian Randman" defer={false}>
-      {props.title && <title>{props.title}</title>}
-      <meta name="description" content={props.description} />
-    </Helmet>
-    <div id="wrapper">
-      <Navigation />
-      <div id="main">
-        {props.children}
+const Main = (props) => {
+  useEffect(() => {
+    const pageURL = window.location.href;
+    const canonicalLink = document.querySelector('link[rel="canonical"]');
+    if (canonicalLink) {
+      canonicalLink.setAttribute('href', pageURL);
+    }
+  }, [window.location.href]);
+
+  return (
+    // <></>
+    <HelmetProvider>
+      <Analytics />
+      {/* <ScrollToTop /> */}
+      <Helmet titleTemplate="%s | Ian Randman" defaultTitle="Ian Randman" defer={false}>
+        {props.title && <title>{props.title}</title>}
+        <meta name="description" content={props.description} />
+      </Helmet>
+      <div id="wrapper">
+        <Navigation />
+        <div id="main">
+          {props.children}
+        </div>
+        {props.fullPage ? null : <SideBar />}
       </div>
-      {props.fullPage ? null : <SideBar />}
-    </div>
-  </HelmetProvider>
-);
+    </HelmetProvider>
+  );
+};
 
 Main.propTypes = {
   children: PropTypes.oneOfType([
